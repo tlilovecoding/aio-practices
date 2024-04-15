4cb5a6bdb12e278ec094db3e52581ba0a347881a
Ting Li <tli@macquariecloudservices.com>
2024-04-15 14:50:01 +1000

    teleout java done

--- java-solutions/solutions/src/main/java/com/aio/aio2023/TeleTrip/teletrip.java
+++ java-solutions/solutions/src/main/java/com/aio/aio2023/TeleTrip/teletrip.java
@@ -1,6 +1,7 @@
 package com.aio.aio2023.TeleTrip;
 
 import java.io.*;
+import java.util.Arrays;
 
 class Solution {
 
@@ -11,6 +12,7 @@ class Solution {
     private static char instructions[] = new char[100005];
 
     private static int answer;
+
     /*
      * Read the next token from the input file.
      * Tokens are separated by whitespace, i.e., spaces, tabs and newlines.
@@ -22,29 +24,60 @@ class Solution {
 
         /* Skip any initial whitespace. */
         next = in.read();
-        while (next >= 0 && Character.isWhitespace((char)next))
+        while (next >= 0 && Character.isWhitespace((char) next))
             next = in.read();
 
         /* Read the following token. */
-        while (next >= 0 && ! Character.isWhitespace((char)next)) {
-            ans.append((char)next);
+        while (next >= 0 && !Character.isWhitespace((char) next)) {
+            ans.append((char) next);
             next = in.read();
         }
 
         return ans.toString();
     }
 
+    private static void calculate() {
+        int[] arr = new int[2 * N + 1];
+        Arrays.fill(arr, 1);
+        arr[N] = 0;
+        int cur = N;
+        for (char c : instructions) {
+            if (c == 'T') {
+                cur = N;
+            }
+            if (c == 'L') {
+                arr[--cur] = 0;
+            }
+            if (c == 'R') {
+                arr[++cur] = 0;
+            }
+        }
+
+        int cnt = 0;
+        for(int i:arr){
+            if(i==0){
+                cnt++;
+            }
+        }
+        answer = cnt;
+    }
+
     public static void main(String[] args) throws IOException {
         /* Open the input and output files. */
+        // BufferedReader input_file = new BufferedReader(new FileReader(
+        // "telein.txt"));
+        // BufferedWriter output_file = new BufferedWriter(new FileWriter(
+        // "teleout.txt"));
+
         BufferedReader input_file = new BufferedReader(new FileReader(
-                "telein.txt"));
+                "C:/Ting/Study/aio-practices/java-solutions/solutions/src/main/java/com/aio/aio2023/TeleTrip/telein.txt"));
         BufferedWriter output_file = new BufferedWriter(new FileWriter(
-                "teleout.txt"));
+                "C:/Ting/Study/aio-practices/java-solutions/solutions/src/main/java/com/aio/aio2023/TeleTrip/teleout.txt"));
 
         /* Read the value of N and the instructions from the input file. */
         N = Integer.parseInt(readToken(input_file));
         instructions = readToken(input_file).toCharArray();
-
+        calculate();
         /*
          * TODO: This is where you should compute your solution. Store the
          * number of different farmhouses that you visit into the variable
