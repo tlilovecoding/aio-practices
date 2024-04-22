e061995f4741e97dd96322cc630533731fc90a23
serenaccli <serena.cc.cat@gmail.com>
2024-04-22 11:46:43 +1000

    fixed clouds over

--- python-solutions/aio2018/cloudCover/cloud.py
+++ python-solutions/aio2018/cloudCover/cloud.py
@@ -4,12 +4,12 @@ N, K = map(int, input_file.readline().split())
 clouds = [int(input_file.readline()) for _ in range(N - 1)]  # Read N-1 cloud cover values
 
 def cloudCover():
-    curr = sum(clouds[:K-1])  # Sum of the first K elements
-    ans = float('inf')
-    for i in range(N-K):  # Adjusted loop range
-        curr += clouds[i + K - 1]
+    curr = sum(clouds[:K])
+    ans = curr
+    for i in range(K, N-1):
+        curr += clouds[i]
+        curr-= clouds[i-K]
         ans = min(curr, ans)
-        curr -= clouds[i]
     return ans
 anz = cloudCover()
 print(anz)
